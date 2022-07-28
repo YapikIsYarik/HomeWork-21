21ea05f2e26f9287b0586ae6d2f121575b46fde6
THEyarik <yaroslavpatcalon@gmail.com>
2022-07-28 20:06:38 +0300

    refactoring and uprage functions genderIcon

--- src/components/Contract.js
+++ src/components/Contract.js
@@ -3,37 +3,29 @@ import './contracts.css'
 import maleIcon from '../assets/img/male.png'
 import femaleIcon from '../assets/img/female.png'
 import contactIcon from '../assets/img/contacts-icon-png-14.jpg'
-function Contract (item) {
 
-        let  contact  = item;
+function Contract(item) {
 
-
-
-
-
-        function genderIcon(){
-            if(contact.item.gender === 'male'){
-                return maleIcon;
-            } else if(contact.item.gender === 'female'){
-                return femaleIcon;
-            }else{
-                return '';
-            }
-        }
-        return (
-            <div>
-                          <div className="contract__item">
-                              <img className='contact__icon' src={contactIcon} alt="people-icon"/>
-                              <div className="contact__body">
-                                  <div className="contact__name">{contact.item.firstName}  {contact.item.lastName} </div>
-                                  <div className="contact__phone"> {contact.item.phone}</div>
-                              </div>
-                              <div className="contact__gender">
-                                  <img className='gender__icon' src={genderIcon()} alt=""/>
-                              </div>
-                          </div>
+    const contact = item;
+    const genderIcon = {
+        male: maleIcon,
+        female: femaleIcon,
+        unknown: '',
+    }
+    return (
+        <div>
+            <div className="contract__item">
+                <img className='contact__icon' src={contactIcon} alt="people-icon"/>
+                <div className="contact__body">
+                    <div className="contact__name">{contact.item.firstName} {contact.item.lastName} </div>
+                    <div className="contact__phone"> {contact.item.phone}</div>
+                </div>
+                <div className="contact__gender">
+                    <img className='gender__icon' src={genderIcon[`${contact.item.gender}`]} alt=""/>
+                </div>
             </div>
-        );
+        </div>
+    );
 
 }
 
